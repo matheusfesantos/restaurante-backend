36c5fff6af3f2a35088d08fa169b7d16de8455c2
Matheus Ferreira <matheusfgsantos31@gmail.com>
2025-10-27 19:46:17 -0300

    first commit

---
+++ src/test/java/com/projeto/projeto_restaurante/ProjetoRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.projeto_restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

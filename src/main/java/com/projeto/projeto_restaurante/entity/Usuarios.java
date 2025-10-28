6c69ce1afaae086443bfc870d044cafc256b6345
Otavio Duque <otavio.a.duque@outlook.com>
2025-10-27 21:15:27 -0300

    Feat(Entitys):Criação das entidades e da tabela dos usuarios no banco, em conjunto com a dependencia do JPA, JWT e Spring Security

---
+++ src/main/java/com/projeto/projeto_restaurante/entity/Usuarios.java
@@ -0,0 +1,54 @@
+package com.projeto.projeto_restaurante.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "usuarios")
+public class Usuarios implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String username;
+    @JsonIgnore
+    private String senha;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
---
+++ src/main/resources/db/migration/V2__tabela_usuarios.sql
@@ -0,0 +1,6 @@
+CREATE TABLE usuarios (
+                          id INT PRIMARY KEY,
+                          nome VARCHAR(80),
+                          username VARCHAR (25),
+                          senha TEXT
+);

2fdbcb2361456226cf244e612076a3adcabb1964
Otavio Duque <otavio.a.duque@outlook.com>
2025-11-01 22:54:19 -0300

    fix(Flyway):Corrigir a criação das primeiras tabelas do banco

--- src/main/resources/db/migration/V2__tabela_usuarios.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE usuarios (
-                          id INT PRIMARY KEY,
-                          nome VARCHAR(80),
-                          username VARCHAR (25),
-                          senha TEXT
-);

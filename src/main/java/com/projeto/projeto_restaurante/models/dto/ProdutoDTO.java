9b6a98957052315331e62de285f08d641d9ac578
Otavio Duque <otavio.a.duque@outlook.com>
2025-11-09 00:47:19 -0300

    Refactor(DTO #7): Fazendo validação dos dados nos DTOS - ProdutoDTO - RegisterDTO

--- src/main/java/com/projeto/projeto_restaurante/models/dto/ProdutoDTO.java
+++ src/main/java/com/projeto/projeto_restaurante/models/dto/ProdutoDTO.java
@@ -1,8 +1,13 @@
 package com.projeto.projeto_restaurante.models.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record ProdutoDTO(
         String foto,
+        @NotBlank(message = "O nome do produto não pode estar em branco")
         String nome,
+        @NotNull(message = "O preco do produto não pode ser nulo")
         Double preco
 ){
 }
--- src/main/java/com/projeto/projeto_restaurante/models/dto/RegisterDTO.java
+++ src/main/java/com/projeto/projeto_restaurante/models/dto/RegisterDTO.java
@@ -1,11 +1,17 @@
 package com.projeto.projeto_restaurante.models.dto;
 
 
-public record RegisterDTO (
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.aspectj.bridge.Message;
 
+public record RegisterDTO (
+        @NotBlank(message = "O nome do usuario não pode estar em branco")
         String nome,
 
+        @NotBlank(message = "O email do usuario não pode estar em branco")
         String email,
 
+        @NotNull(message = "A senha do ususario não pode estar em branco")
         String senha)
 { }
